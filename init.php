44378c9b27fb09abb5d54c24fb423d62c9e7e6e1
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-15 10:02:39 -0400

    prevent creating the class aliases when the original plugin is still installed

--- init.php
+++ init.php
@@ -1,11 +1,13 @@
 <?php
 
-class_alias(Winter\Sitemap\Plugin::class, RainLab\Sitemap\Plugin::class);
+if (!class_exists(RainLab\Sitemap\Plugin::class)) {
+    class_alias(Winter\Sitemap\Plugin::class, RainLab\Sitemap\Plugin::class);
 
-class_alias(Winter\Sitemap\Classes\DefinitionItem::class, RainLab\Sitemap\Classes\DefinitionItem::class);
+    class_alias(Winter\Sitemap\Classes\DefinitionItem::class, RainLab\Sitemap\Classes\DefinitionItem::class);
 
-class_alias(Winter\Sitemap\Controllers\Definitions::class, RainLab\Sitemap\Controllers\Definitions::class);
+    class_alias(Winter\Sitemap\Controllers\Definitions::class, RainLab\Sitemap\Controllers\Definitions::class);
 
-class_alias(Winter\Sitemap\FormWidgets\SitemapItems::class, RainLab\Sitemap\FormWidgets\SitemapItems::class);
+    class_alias(Winter\Sitemap\FormWidgets\SitemapItems::class, RainLab\Sitemap\FormWidgets\SitemapItems::class);
 
-class_alias(Winter\Sitemap\Models\Definition::class, RainLab\Sitemap\Models\Definition::class);
+    class_alias(Winter\Sitemap\Models\Definition::class, RainLab\Sitemap\Models\Definition::class);
+}
