de7baeafa850cb2c5fcbb53f446a46a3279c3281
Ben Thomson <git@alfreido.com>
2021-03-16 23:39:41 +0800

    Clean up aliases

--- init.php
+++ init.php
@@ -1,13 +1,3 @@
 <?php
-
-if (!class_exists(RainLab\Sitemap\Plugin::class)) {
-    class_alias(Winter\Sitemap\Plugin::class, RainLab\Sitemap\Plugin::class);
-
-    class_alias(Winter\Sitemap\Classes\DefinitionItem::class, RainLab\Sitemap\Classes\DefinitionItem::class);
-
-    class_alias(Winter\Sitemap\Controllers\Definitions::class, RainLab\Sitemap\Controllers\Definitions::class);
-
-    class_alias(Winter\Sitemap\FormWidgets\SitemapItems::class, RainLab\Sitemap\FormWidgets\SitemapItems::class);
-
-    class_alias(Winter\Sitemap\Models\Definition::class, RainLab\Sitemap\Models\Definition::class);
-}
+// Include aliases
+require_once __DIR__ . '/aliases.php';
