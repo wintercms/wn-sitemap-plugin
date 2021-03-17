b9478c41dc17a0f3b20e9613414523352091535c
Marc Jauvin <marc.jauvin@gmail.com>
2021-03-17 16:04:52 -0400

    Use new lazy Class Loader aliasing (#2)

--- aliases.php
+++ aliases.php
@@ -1,18 +1,17 @@
 <?php
+
+use Winter\Storm\Support\ClassLoader;
+
 /**
  * To allow compatibility with plugins that extend the original RainLab.Sitemap plugin, this will alias those classes to
  * use the new Winter.Sitemap classes.
  */
 $aliases = [
-    Winter\Sitemap\Plugin::class                   => 'RainLab\Sitemap\Plugin',
-    Winter\Sitemap\Classes\DefinitionItem::class   => 'RainLab\Sitemap\Classes\DefinitionItem',
-    Winter\Sitemap\Controllers\Definitions::class  => 'RainLab\Sitemap\Controllers\Definitions',
-    Winter\Sitemap\FormWidgets\SitemapItems::class => 'RainLab\Sitemap\FormWidgets\SitemapItems',
-    Winter\Sitemap\Models\Definition::class        => 'RainLab\Sitemap\Models\Definition',
+    Winter\Sitemap\Plugin::class                   => RainLab\Sitemap\Plugin::class,
+    Winter\Sitemap\Classes\DefinitionItem::class   => RainLab\Sitemap\Classes\DefinitionItem::class,
+    Winter\Sitemap\Controllers\Definitions::class  => RainLab\Sitemap\Controllers\Definitions::class,
+    Winter\Sitemap\FormWidgets\SitemapItems::class => RainLab\Sitemap\FormWidgets\SitemapItems::class,
+    Winter\Sitemap\Models\Definition::class        => RainLab\Sitemap\Models\Definition::class,
 ];
 
-foreach ($aliases as $original => $alias) {
-    if (!class_exists($alias)) {
-        class_alias($original, $alias);
-    }
-} 
+app(ClassLoader::class)->addAliases($aliases);
