a943ce2893ce9404fd31638aba6643ab252d562c
Luke Towers <github@luketowers.ca>
2023-07-31 01:31:27 -0600

    Handle requests for the sitemap.xsl stylesheet

--- routes.php
+++ routes.php
@@ -5,19 +5,22 @@ use Cms\Classes\Controller;
 use Winter\Sitemap\Models\Definition;
 use Illuminate\Database\Eloquent\ModelNotFoundException as ModelNotFound;
 
-Route::get('sitemap.xml', function()
+Route::get('sitemap.xml', function ()
 {
     $themeActive = Theme::getActiveTheme()->getDirName();
 
     try {
         $definition = Definition::where('theme', $themeActive)->firstOrFail();
-    }
-    catch (ModelNotFound $e) {
+    } catch (ModelNotFound $e) {
         Log::info(trans('winter.sitemap::lang.definition.not_found'));
-
         return App::make(Controller::class)->setStatusCode(404)->run('/404');
     }
 
     return Response::make($definition->generateSitemap())
         ->header('Content-Type', 'application/xml');
+});
+
+Route::get('sitemap.xsl', function () {
+    return Response::make(file_get_contents(plugins_path('winter/sitemap/assets/sitemap.xsl')))
+        ->header('Content-Type', 'text/xsl');
 });
