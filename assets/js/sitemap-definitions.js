fd6c2e33597908b3d261941a3eaead517fc40035
Marc Jauvin <marc.jauvin@gmail.com>
2024-10-09 01:58:49 -0400

    Fix saving sitemap definitions (#19)
    Fixes #15
    Addresses wintercms/winter#1219, wintercms/winter#1222
    Replaces #18

--- assets/js/sitemap-definitions.js
+++ assets/js/sitemap-definitions.js
@@ -49,7 +49,8 @@
             return result
         }
 
-        data.options.data['itemData'] = iterator($items)
+        const fieldName = $('div[data-control=sitemap-item-editor]').data('fieldName')
+        data.options.data[fieldName] = iterator($items);
     }
 
     $(document).ready(function(){
